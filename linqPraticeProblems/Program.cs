40ab7e16d6b4f92671e5955f27df4ab4b8b955f4
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-03-09 10:52:02 -0500

    this is the first attemt to have a pair reop

---
+++ linqPraticeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linqPraticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
