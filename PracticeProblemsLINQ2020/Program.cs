9bfdc31dbbf584ef0251923e2fec8fdfa7e13efc
unknown <christinesoto21@gmail.com>
2020-03-09 11:11:48 -0500

    Initial

---
+++ PracticeProblemsLINQ2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemsLINQ2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
